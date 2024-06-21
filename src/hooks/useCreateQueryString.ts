7538dd4fb26311c3ce07c68e4dd0612942f7ce8f
luke.lin <milletbard@gmail.com>
2024-06-21 18:45:09 +0800

    feat: implement useCreateQueryString hook

---
+++ src/hooks/useCreateQueryString.ts
@@ -0,0 +1,20 @@
+import { useSearchParams } from "next/navigation";
+import React, { useCallback } from "react";
+
+const useCreateQueryString = () => {
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  return createQueryString;
+};
+
+export default useCreateQueryString;
